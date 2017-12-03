72fb748267c2c58edfa3e4532a8dae49c7580ba3
DerekG <dgan@umich.edu>
2017-12-03 17:31:25 -0500

    Preliminary caching pattern

--- Final Project.py
+++ Final Project.py
@@ -4,3 +4,12 @@ import collections
 import tweepy
 import json
 import sqlite3
+
+
+try:
+    cache_file = open(CACHE_FNAME,'r')
+    cache_contents = cache_file.read()
+    cache_file.close()
+    CACHE_DICTION = json.loads(cache_contents)
+except:
+    CACHE_DICTION = {}
